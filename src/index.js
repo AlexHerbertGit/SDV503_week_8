d58873fac0516128a9fa8a9f9c5188a5f8d73be9
AlexHerbertGit <alex.herbert.tech@gmail.com>
2023-04-24 12:42:23 +1200

    Rock, Paper, Scissors game function created

--- src/index.js
+++ src/index.js
@@ -8,3 +8,23 @@ function compareTwoStrings(strOne, strTwo) {
 }
 
 console.log(compareTwoStrings("AB", "CD"))
+
+function playGame(playerSelection) {
+    const options = ["rock", "paper", "scissors"];
+    const computerSelection = options[Math.floor(Math.random() * options.length)];
+
+    if (playerSelection === computerSelection) {
+        return "It's a tie!";
+    } else if (
+        (playerSelection === "rock" && computerSelection === "scissors") ||
+        (playerSelection === "paper" && computerSelection === "rock") ||
+        (playerSelection === "scissors" && computerSelection === "paper")
+    ) {
+        return "You Win! " + playerSelection + " beats" + computerSelection;
+    } else {
+        return "You Loose!" + computerSelection + " beats" + playerSelection;
+    }
+}
+
+console.log(playGame("scissors"))
+console.log(playGame("paper"))
