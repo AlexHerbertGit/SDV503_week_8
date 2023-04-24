ae559f6101bc8a9491c4646f6a55e89dd6a3c5e2
AlexHerbertGit <alex.herbert.tech@gmail.com>
2023-04-24 13:16:34 +1200

    Added Comments to the code

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ console.log(compareTwoStrings("AB", "CD"))
 function playGame(playerSelection) {
     const options = ["rock", "paper", "scissors"];
     const computerSelection = options[Math.floor(Math.random() * options.length)];
-
+//Computer uses the length of the options array and picks a value at random to select an option. This is then passed down to the next part of the function.
     if (playerSelection === computerSelection) {
         return "It's a tie!";
     } else if (
@@ -20,11 +20,11 @@ function playGame(playerSelection) {
         (playerSelection === "paper" && computerSelection === "rock") ||
         (playerSelection === "scissors" && computerSelection === "paper")
     ) {
-        return "You Win! " + playerSelection + " beats" + computerSelection;
+        return "You Win! " + playerSelection + " beats " + computerSelection;
     } else {
-        return "You Loose!" + computerSelection + " beats" + playerSelection;
+        return "You Loose!" + computerSelection + " beats " + playerSelection;
     }
 }
-
+//These if and else statements determine the win, tie or loss conditions based on what the player selection is vs the computers random selection.
 console.log(playGame("scissors"))
 console.log(playGame("paper"))
